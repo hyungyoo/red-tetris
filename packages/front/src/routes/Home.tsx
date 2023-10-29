4654390c97e7c662ba20dced6208f599ffc793fc
jeanoza <choikb916@gmail.com>
2023-10-29 15:35:52 +0100

    before pr

--- packages/front/src/routes/Home.tsx
+++ packages/front/src/routes/Home.tsx
@@ -1,11 +1,29 @@
-import Block from '../components/Block'
 import JoinForm from './home/JoinForm'
+import RoomList, { Room } from './home/RoomList'
+
+const TEST_ROOMS: Room[] = [
+  {
+    id: 1,
+    name: 'room1',
+    players: ['hyungyoo', 'kychoi']
+  },
+  {
+    id: 2,
+    name: 'room2',
+    players: ['seyun', 'dolee']
+  },
+  {
+    id: 3,
+    name: 'room3',
+    players: ['sucho', 'cjung-mo']
+  }
+]
 
 function Home() {
   return (
     <div className='flex justify-center w-full'>
       <JoinForm />
-      <div>list</div>
+      <RoomList rooms={TEST_ROOMS} />
     </div>
   )
 }
