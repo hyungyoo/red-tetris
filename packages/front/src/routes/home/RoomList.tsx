83e394b9482f39f4b5ec4dc6fa261c713fc3b54b
jeanoza <choikb916@gmail.com>
2023-11-05 20:56:21 +0100

    feat: add no room case and tweaks

--- packages/front/src/routes/home/RoomList.tsx
+++ packages/front/src/routes/home/RoomList.tsx
@@ -10,14 +10,14 @@ function RoomList(props: RoomListProps) {
   const { rooms } = props
   return (
     <Section title='Room List'>
-      <ul className='w-full'>
-        {rooms &&
+      <ul className='w-96'>
+        {rooms.length === 0 && <li className='text-center'>No room available</li>}
+        {rooms.length > 0 &&
           rooms.map((room, i) => (
             <li key={`room[${i}]`} className='flex justify-between w-full'>
-              <Section title={room?.name}>
-                <ul className='w-96'>
-                  {room.players?.length &&
-                    room.players.map((p: any, j: any) => <li key={`player_name[${j}]`}>{p?.name}</li>)}
+              <Section title={room.name}>
+                <ul className='w-80'>
+                  {room.players.length > 0 && room.players.map((p, j) => <li key={`player_name[${j}]`}>{p.name}</li>)}
                 </ul>
               </Section>
             </li>
