9b5356f463600022154fd76ee35d61c412d3d1c4
jeanoza <choikb916@gmail.com>
2023-11-08 17:54:15 +0100

    fix: es6 instead of es5 to avoid error

--- packages/front/src/components/Layout.tsx
+++ packages/front/src/components/Layout.tsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from 'react'
+
+//TODO: to implement later dark/light toggle
+function Navbar() {
+  const [darkToggle, setDarkToggle] = useState(false)
+
+  useEffect(() => {
+    console.log('useeffect')
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [darkToggle])
+  function toggleDarkMode() {
+    localStorage.theme = darkToggle ? 'light' : 'dark'
+    console.log(darkToggle)
+    setDarkToggle(!darkToggle)
+  }
+  return (
+    <nav className='w-full flex justify-between z-10 absolute top-0 left-0'>
+      <div
+        onClick={() => {
+          console.log('HERE')
+        }}
+      >
+        RED Tetris
+      </div>
+      <div className={`w-10 h-full border rounded-full m-2`} onClick={toggleDarkMode}>
+        <div
+          className={`w-4 h-4 bg-neutral-900 dark:bg-white rounded-full m-0.5 ${darkToggle ? 'translate-x-4' : ''}`}
+        ></div>
+      </div>
+    </nav>
+  )
+}
 function Layout({ children }: React.PropsWithChildren) {
   return (
     <div className='w-full h-screen relative dark:bg-neutral-900 dark:text-white'>
+      <Navbar />
       <div className='w-full h-screen absolute top-0 left-0 p-32'>{children}</div>
     </div>
   )
